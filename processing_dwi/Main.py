11f750919fe5761f696a6515e1de53f08c858f6e
Rita Oliveira <o_anarita@hotmail.com>
2025-01-14 11:18:18 +0100

    teste

--- processing_dwi/Main.py
+++ processing_dwi/Main.py
@@ -15,7 +15,7 @@ os.system('clear')
 os.system('cls')
 
 # from bids_structure import *
-# importlib.reload(sys.modules['bids_structure'])
+
 # from bids_structure import BIDSStructure
 
 # teste
@@ -24,11 +24,13 @@ os.system('cls')
 sys.path.append(os.path.join(os.path.expanduser('~'),  'Documents', 'Rita','Codes_GitHub','dMRSI','processing_dwi'))
 sys.path.append(os.path.join(os.path.expanduser('~'),  'Documents', 'Rita','Codes_GitHub','dMRSI'))
 
-from Step1_fill_study_excel import *
-from Step2_raw2nii2bids import *
-from Step2_correct_orientation import *
+#from Step1_fill_study_excel import *
+#from Step2_raw2nii2bids import *
+#from Step2_correct_orientation import *
 from Step3_preproc import *
+
 from bids_structure import *
+from custom_functions import *
 
 importlib.reload(sys.modules['custom_functions'])
 importlib.reload(sys.modules['bids_structure'])
@@ -46,12 +48,12 @@ cfg['common_folder']        = os.path.join(os.path.expanduser('~'), 'Documents',
 cfg['atlas']                = 'Atlas_WHS_v4'
 
 #### STEP 1. COHORT DEFINITION >>> Use Base env
-Step1_fill_study_excel(cfg)   ## Do once or if new data is added to the excel study file
+#Step1_fill_study_excel(cfg)   ## Do once or if new data is added to the excel study file
 
 
 #### STEP 2. NIFTI CONVERT SUBJECT >>> Use Dicomifier env
-Step2_raw2nii2bids(subj_list,cfg) # Do once for subject
-Step2_correct_orientation(subj_list,cfg) # Do once for subject
+#Step2_raw2nii2bids(subj_list,cfg) # Do once for subject
+#Step2_correct_orientation(subj_list,cfg) # Do once for subject
 
 #### STEP 3. PREPROCESS SUBJECT >>> Use Base env
 cfg['do_topup']         = 0
@@ -60,17 +62,17 @@ Step3_preproc(subj_list,cfg) ### Do more than once if needed
 
 
 #### STEP 4. MODELLING SUBJECT >>> Use Swissknife env
-from Step4_modelling_GM import *
-cfg['model_list_GM'] =  ['Sandi','Sandix']
-Step4_modelling_GM(subj_list,cfg) ### Do more than once if needed
+#from Step4_modelling_GM import *
+#cfg['model_list_GM'] =  ['Sandi','Sandix']
+#Step4_modelling_GM(subj_list,cfg) ### Do more than once if needed
 
-from Step4_modelling_WM import *
-cfg['model_list_WM'] =  ['SMI']
-Step4_modelling_WM(subj_list,cfg) ### Do more than once if needed
+#from Step4_modelling_WM import *
+#cfg['model_list_WM'] =  ['SMI']
+#Step4_modelling_WM(subj_list,cfg) ### Do more than once if needed
 
 
 #### STEP 5. GET VALUES - not finished yet
-from Step5_GetEstimates import *
-cfg['model_list'] = ['Nexi']
-cfg['ROIs']       = ['Primary visual area']
-Step5_GetEstimates(subj_list,cfg) ### Do more than once if needed
+#from Step5_GetEstimates import *
+#cfg['model_list'] = ['Nexi']
+#cfg['ROIs']       = ['Primary visual area']
+#Step5_GetEstimates(subj_list,cfg) ### Do more than once if needed
