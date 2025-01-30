537ac2108c7853b939e3be38ad97af8df5b6ec4c
Rita Oliveira <o_anarita@hotmail.com>
2025-01-30 13:09:51 +0100

    change name of folder of the dwi from all deltas together

--- processing_dwi/Main.py
+++ processing_dwi/Main.py
@@ -15,9 +15,9 @@ plt.close('all');
 os.system('clear')
 os.system('cls')
 
-# from custom_functions import QA_brain_extract
-# importlib.reload(sys.modules['custom_functions'])
-# from custom_functions import QA_brain_extract
+from custom_functions import ants_apply_transforms
+importlib.reload(sys.modules['custom_functions'])
+from custom_functions import ants_apply_transforms
 
 ############################## ADD CODE PATH ##############################
 sys.path.append(os.path.join(os.path.expanduser('~'),  'Documents', 'Rita','Codes_GitHub','dMRSI','processing_dwi'))
@@ -36,12 +36,12 @@ subj_list = ['sub-01']
 
 cfg                         = {}
 cfg['data_path']            = os.path.join(os.path.expanduser('~'), 'Documents','Rita','Data','dMRI_Pilot_20220116')
-cfg['data_path']            = os.path.join(os.path.expanduser('~'), 'Documents','Rita','Data','dMRI_Pilot_20220121')
+#cfg['data_path']            = os.path.join(os.path.expanduser('~'), 'Documents','Rita','Data','dMRI_Pilot_20220121')
 
-cfg['prep_foldername']      = 'preprocessed_shortscan'
-cfg['analysis_foldername']  = 'analysis_shortscan'
+cfg['prep_foldername']      = 'preprocessed'
+cfg['analysis_foldername']  = 'analysis'
 cfg['common_folder']        = os.path.join(os.path.expanduser('~'), 'Documents','Rita','Data','common')
-cfg['scan_list_name']       = 'ScanList_shortscan.xlsx'
+cfg['scan_list_name']       = 'ScanList.xlsx'
 cfg['atlas']                = 'Atlas_WHS_v4'
 
     
@@ -67,7 +67,7 @@ cfg['redo_gibbs']           = 0
 cfg['redo_topup']           = 0
 cfg['redo_eddy']            = 0
 cfg['redo_final_mask']      = 0
-cfg['preproc_type']         = 'individual' #  'individual' or'combined'
+cfg['preproc_type']         = 'combined' #  'individual' or'combined'
 cfg['model_list_GM']        =  ['Nexi','Sandi']
 cfg['model_list_WM']        =  ['SMI']
 #from Step3_preproc import *
@@ -83,7 +83,7 @@ Step4_modelling_WM(subj_list,cfg) ### Do more than once if needed
 
 
 #### STEP 5. GET VALUES - not finished yet
-#from Step5_GetEstimates import *
-#cfg['ROIs_GM']       = ['Primary visual area']
-#cfg['ROIs_WM']       = ['Primary visual area']
-#Step5_GetEstimates(subj_list,cfg) ### Do more than once if needed
+from Step5_GetEstimates import *
+cfg['ROIs_GM']       = ['Dentate gyrus']
+cfg['ROIs_WM']       = ['Primary visual area']
+Step5_GetEstimates(subj_list,cfg) ### Do more than once if needed
