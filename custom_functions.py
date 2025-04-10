de574c1d1f54cb0b72cb9eeeb52c34f049347c28
Rita Oliveira <o_anarita@hotmail.com>
2025-04-10 10:33:09 +0200

    change name of STE bvecs file

--- custom_functions.py
+++ custom_functions.py
@@ -966,7 +966,7 @@ def extract_methods(methods_in, bids_strc, acqp, cfg=None):
 
         np.savetxt(bids_strc.get_path('bvalsNom.txt'), bvals_nom, delimiter=' ', fmt='%.1f')
         np.savetxt(bids_strc.get_path('bvalsEff.txt'), bvals_eff, delimiter=' ', fmt='%.1f')
-        np.savetxt(bids_strc.get_path('STE_bvecs_fake.txt'), bvecs_fake, delimiter=' ', fmt='%.1f')
+        np.savetxt(bids_strc.get_path('bvecs_fake.txt'), bvecs_fake, delimiter=' ', fmt='%.1f')
 
 
 ##### IMAGE OPERATIONS - HANDLING #####
