da415855a2ffab9141936a034d095b34a9729886
Rita Oliveira <o_anarita@hotmail.com>
2025-04-25 11:21:49 +0200

    improve plot fot STE vs LTE and regions of interest. Registration is still being improved

--- analysis_dwi/STE_vs_LTE.py
+++ analysis_dwi/STE_vs_LTE.py
@@ -35,8 +35,9 @@ cfg['analysis_foldername']  = 'analysis'
 cfg['common_folder']        = os.path.join(os.path.expanduser('~'), 'Documents','Rita','Data','common')
 cfg['scan_list_name']       = 'ScanList.xlsx'
 cfg['atlas']                = 'Atlas_WHS_v4'
-cfg['ROIs_GM']              = ['hippocampus','M1','M2','S1','S2', 'V1', 'PL','CG', 'Thal', 'CC']
-
+cfg['ROIs']                 = ['hippocampus','M1','M2','S1','S2', 'V1', 'PL','CG', 'Thal', 'CC']
+cfg['ROIs']                 = ['CC','Thal','hippocampus','M1','CSF']
+  
 subj_list = ['sub-01']
 import pandas as pd
 import glob
@@ -58,11 +59,11 @@ for subj in subj_list:
     
     # Handle dwi data
     bids_LTE      = create_bids_structure(subj=subj, sess=1, datatype='dwi', root=cfg['data_path'] , 
-                 folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description='Nexi')
-    extract_vols(os.path.join(bids_LTE.get_path(),'powderaverage_dwi.nii.gz'), bids_LTE.get_path('b0.nii.gz'), 0, 1)
+                 folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description='pwd_avg_Delta_15')
+    extract_vols(os.path.join(bids_LTE.get_path(),'pwd_avg_norm.nii.gz'), bids_LTE.get_path('b0.nii.gz'), 0, 1)
 
     # Get atlas in dwi space and atlas labels
-    bids_strc_reg  = create_bids_structure(subj=subj, sess=1, datatype='registration', description='allDelta-allb', root=cfg['data_path'] , 
+    bids_strc_reg  = create_bids_structure(subj=subj, sess=1, datatype='registration', description='Delta_15_fwd', root=cfg['data_path'] , 
                                  folderlevel='derivatives', workingdir=cfg['analysis_foldername'])
     atlas = bids_strc_reg.get_path('atlas_in_dwi.nii.gz')
     atlas_labels = pd.read_csv(
@@ -73,13 +74,15 @@ for subj in subj_list:
          names=['IDX', 'R', 'G', 'B', 'A', 'VIS', 'MSH', 'LABEL'], 
          quotechar='"',)  
     
-    bvals_LTE = read_numeric_txt(os.path.join(bids_LTE.get_path(),'powderaverage.bval'))
-    S_S0_LTE  = nib.load(os.path.join(bids_LTE.get_path(),'powderaverage_dwi.nii.gz')).get_fdata()    
- 
+    #bvals_LTE = read_numeric_txt(os.path.join(bids_LTE.get_path(),'powderaverage.bval'))
+    bvals_LTE = read_numeric_txt(find_files_with_pattern(bids_LTE,'bvalsNom')[0])
+    #S_S0_LTE  = nib.load(os.path.join(bids_LTE.get_path(),'pwd_avg_norm.nii.gz')).get_fdata()    
+    S_S0_LTE  = nib.load(find_files_with_pattern(bids_LTE,'pwd_avg_norm.nii.gz')[0]).get_fdata()
+   
     # List of acquisition sessions
     sess_list    = [x for x in list(subj_data['blockNo'].unique()) if not math.isnan(x)] # clean NaNs
     
-    fig, axs = plt.subplots(1, len( cfg['ROIs_GM']), figsize=(12, 4))  
+    fig, axs = plt.subplots(1, len( cfg['ROIs']), figsize=(12, 4))  
     fig.subplots_adjust(wspace=0.05,hspace=0.02, top=0.90, bottom=0.14, left=0.09, right=0.95)  
     for sess in sess_list:
 
@@ -108,7 +111,7 @@ for subj in subj_list:
            
             ### PLOT Signal FOR EACH ROI ###
             k=0
-            for ROI in cfg['ROIs_GM']:
+            for ROI in cfg['ROIs']:
  
                 mask_indexes = create_ROI_mask(atlas, atlas_labels, ROI, bids_strc_reg)
                 
@@ -121,22 +124,29 @@ for subj in subj_list:
                         S_S0_masked[:, :, :, v] = np.multiply(S_S0_masked[:, :, :, v], mask_indexes)
                     LTE = S_S0_masked.reshape(S_S0_masked.shape[0]*S_S0_masked.shape[1]*S_S0_masked.shape[2], S_S0_masked.shape[3])
                     LTE = LTE[~(np.isnan(LTE).any(axis=1) | (LTE == 0).any(axis=1))]
-                    mask = bvals_LTE < 2.5
+                    mask = bvals_LTE < 8
                     filtered_bvals = bvals_LTE[mask]
+                    mask_diff = np.arange(0, 5)
+                    filtered_bvals = filtered_bvals[mask_diff]
                     filtered_bvals = np.insert(filtered_bvals, 0, 0)
                     filtered_LTE = np.nanmean(LTE, axis=0)
                     filtered_LTE = np.expand_dims(filtered_LTE, axis=0)  
                     filtered_LTE = filtered_LTE[mask]
+                    filtered_LTE = filtered_LTE[mask_diff]
                     filtered_LTE = np.insert(filtered_LTE, 0, 1)
                     
+                    data = np.log(filtered_LTE)
+                    #data = filtered_LTE
+                     
                     # Plot data
-                    axs[k].plot(np.transpose(filtered_bvals), np.log(filtered_LTE), 'ko', markersize=3)
+                    axs[k].plot(np.transpose(filtered_bvals), data, 'ko', markersize=3)
 
                     # Fit LTE 
-                    m, b = np.polyfit(np.transpose(filtered_bvals), np.log(filtered_LTE), 1)
-                    model = np.poly1d(np.polyfit(np.transpose(filtered_bvals), np.log(filtered_LTE), 1))
-                    b_fit = np.linspace(0, 3, 100)  # Smooth b-values 
-                    axs[k].plot(b_fit, m*b_fit+b, linestyle="--", color="black",label='_nolegend_')
+                    coeffs = np.polyfit(np.transpose(filtered_bvals), data, 2)
+                    model = np.poly1d(coeffs)
+                    b_fit = np.linspace(0,bvals_LTE.max(), 100)  # Smooth b-values 
+                    y_fit = model(b_fit)
+                    axs[k].plot(b_fit, y_fit, linestyle="--", color="black",label='_nolegend_')
 
       
                 # STE signal
@@ -152,35 +162,37 @@ for subj in subj_list:
                 filtered_STE = np.expand_dims(filtered_STE, axis=0)  
                 filtered_STE = np.insert(filtered_STE, 0, 1)
                 
+                data = np.log(filtered_STE)
+                #data = filtered_STE
                 
                 # Fit STE 
-                popt, pcov = curve_fit(linear_model, np.transpose(filtered_bvals), np.log(filtered_STE)) 
+                popt, pcov = curve_fit(linear_model, np.transpose(filtered_bvals), data) 
                 b_fit = np.linspace(0, 3, 100)  # Smooth b-values 
                 signal_fit = linear_model(b_fit, *popt)
                 
                 if sess == 1:
-                    axs[k].plot(np.transpose(filtered_bvals), np.log(filtered_STE), 'bo', markersize=3)
+                    axs[k].plot(np.transpose(filtered_bvals), data, 'bo', markersize=3)
                     #axs[k].plot(np.transpose(bvals_STE), (np.nanmean(STE, axis=0)), 'bo', markersize=3)
                     axs[k].plot(b_fit, signal_fit, linestyle="--", color="blue",label='_nolegend_')
 
                 if sess == 2:
-                    axs[k].plot(np.transpose(filtered_bvals), np.log(filtered_STE), 'ro', markersize=3)
+                    axs[k].plot(np.transpose(filtered_bvals), data, 'ro', markersize=3)
                     #axs[k].plot(np.transpose(bvals_STE), (np.nanmean(STE, axis=0)), 'ro', markersize=3)
                     axs[k].plot(b_fit, signal_fit, linestyle="--", color="red",label='_nolegend_')
 
                 
                 # Set axes
                 if k==0:
-                    axs[k].set_ylabel(r'ln($S / S_0$)', fontdict={'size': 12, 'weight': 'bold', 'style': 'italic'})  
+                    axs[k].set_ylabel(r'$S / S_0$', fontdict={'size': 12, 'weight': 'bold', 'style': 'italic'})  
                     axs[k].legend(['LTE','STE - 16 avg','STE - 64 avg'], loc='upper right',prop={'size': 6})
                 axs[k].set_xlabel('b-val', fontdict={'size': 12, 'style': 'italic'})
                 axs[k].grid(True)
                 axs[k].set_title(ROI)
-                axs[k].set_xlim([-0.5, 2.6])
-                axs[k].set_ylim([-1.6, 0.05])
+                axs[k].set_xlim([-0.5, 3])
+                #axs[k].set_ylim([-1.6, 0.05])
                 if k != 0:  # Remove y-axis labels for all but the first subplot
                     axs[k].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-                axs[k].set_xticks([ 1, 2])
+                #axs[k].set_xticks(np.unique(bvals_LTE))
                 
              
                 k += 1
