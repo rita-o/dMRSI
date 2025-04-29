5224b08fe64ebc747b73d716f4a8ba9cb5d06504
Rita Oliveira <o_anarita@hotmail.com>
2025-04-29 17:29:01 +0200

    imrove script to chose STE vs LTE

--- analysis_dwi/STE_vs_LTE.py
+++ analysis_dwi/STE_vs_LTE.py
@@ -49,6 +49,7 @@ create_directory(output_folder)
 ################# Compare random order b vals acquisition ####################
 
 scan_list   = pd.read_excel(os.path.join(cfg['data_path'] , 'ScanList.xlsx'))
+data_type_LTE ='Delta_15'
 
 for subj in subj_list:
     
@@ -57,146 +58,183 @@ for subj in subj_list:
     # Extract data for subject
     subj_data    = scan_list[scan_list['newstudyName'] == subj].reset_index(drop=True)
     
-    # Handle dwi data
-    bids_LTE      = create_bids_structure(subj=subj, sess=1, datatype='dwi', root=cfg['data_path'] , 
-                 folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description='pwd_avg_Delta_15')
-    extract_vols(os.path.join(bids_LTE.get_path(),'pwd_avg_norm.nii.gz'), bids_LTE.get_path('b0.nii.gz'), 0, 1)
-
-    # Get atlas in dwi space and atlas labels
-    bids_strc_reg  = create_bids_structure(subj=subj, sess=1, datatype='registration', description='Delta_15_fwd', root=cfg['data_path'] , 
-                                 folderlevel='derivatives', workingdir=cfg['analysis_foldername'])
-    atlas = bids_strc_reg.get_path('atlas_in_dwi.nii.gz')
-    atlas_labels = pd.read_csv(
-         glob.glob(os.path.join(cfg['common_folder'], cfg['atlas'], '*atlas.label'))[0],
-         sep=r'\s+',
-         skiprows=14,  
-         header=None,  
-         names=['IDX', 'R', 'G', 'B', 'A', 'VIS', 'MSH', 'LABEL'], 
-         quotechar='"',)  
-    
-    #bvals_LTE = read_numeric_txt(os.path.join(bids_LTE.get_path(),'powderaverage.bval'))
-    bvals_LTE = read_numeric_txt(find_files_with_pattern(bids_LTE,'bvalsNom')[0])
-    #S_S0_LTE  = nib.load(os.path.join(bids_LTE.get_path(),'pwd_avg_norm.nii.gz')).get_fdata()    
-    S_S0_LTE  = nib.load(find_files_with_pattern(bids_LTE,'pwd_avg_norm.nii.gz')[0]).get_fdata()
-   
     # List of acquisition sessions
     sess_list    = [x for x in list(subj_data['blockNo'].unique()) if not math.isnan(x)] # clean NaNs
     
-    fig, axs = plt.subplots(1, len( cfg['ROIs']), figsize=(12, 4))  
-    fig.subplots_adjust(wspace=0.05,hspace=0.02, top=0.90, bottom=0.14, left=0.09, right=0.95)  
-    for sess in sess_list:
-
-            # Create BIDS structures
-            bids_STE      = create_bids_structure(subj=subj, sess=sess, datatype='dwi_STE', root=cfg['data_path'] , 
-                         folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description='pwd_avg')
-            extract_vols(find_files_with_pattern(bids_STE,'pwd_avg_norm.nii.gz')[0], bids_STE.get_path('b0.nii.gz'), 0, 1)
-
-            # REGISTRATION 
-            # Register STE --> dwi
-            # antsreg_simple(bids_dwi.get_path('b0.nii.gz'),  # fixed
-            #         bids_STE.get_path('b0.nii.gz'),# moving
-            #         os.path.join(out_path,'STE2dwi'))
+    sess_list = [1] # rita
     
-            # # Apply inverse transform to put T2w in dwi space
-            # ants_apply_transforms_simple([bids_STE.get_path('b0.nii.gz')],  # input
-            #                     bids_dwi.get_path('b0.nii.gz'),# reference
-            #                     [bids_STE.get_path('b0.nii.gz').replace('.nii.gz','_in_dwi.nii.gz')],  # output
-            #                     os.path.join(out_path,'STE2dwi0GenericAffine.mat'))  # transform 1
+    for sess in sess_list:
+        print('Getting model estimates on session ' + str(sess) + '...')
+    
+        
+        ### Handle LTE data ###
+        bids_LTE      = create_bids_structure(subj=subj, sess=1, datatype='dwi', root=cfg['data_path'] , 
+                     folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description=f'pwd_avg_{data_type_LTE}')
+        bvals_LTE = read_numeric_txt(find_files_with_pattern(bids_LTE,'bvalsNom')[0])
+        S_S0_LTE  = nib.load(find_files_with_pattern(bids_LTE,'pwd_avg_norm.nii.gz')[0]).get_fdata()
+        
+        ### Get MD ###
+        bids_temp      = create_bids_structure(subj=subj, sess=1, datatype='dwi', root=cfg['data_path'] , 
+                     folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description=f'DTI_DKI_{data_type_LTE}')
+        bids_temp.set_param(base_name='')
+        MD = nib.load(find_files_with_pattern(bids_temp,'md_dti')[0]).get_fdata()
 
-           
-            # load data
-            bvals_STE = read_numeric_txt(find_files_with_pattern(bids_STE,'bvalsNom')[0])
-            S_S0_STE  = nib.load(find_files_with_pattern(bids_STE,'pwd_avg_norm.nii.gz')[0]).get_fdata()
+        
+        ###  Handle STE data and register them to LTE using previously computed transforms ### 
+        bids_STE      = create_bids_structure(subj=subj, sess=sess, datatype='dwi_STE', root=cfg['data_path'] , 
+                     folderlevel='derivatives', workingdir=cfg['analysis_foldername'],description='pwd_avg')
+        bids_LTE_prep      = create_bids_structure(subj=subj, sess=sess, datatype='dwi', root=cfg['data_path'] , 
+                      folderlevel='derivatives', workingdir=cfg['prep_foldername'],description=data_type_LTE+'_fwd')
+        bids_STE_prep      = create_bids_structure(subj=subj, sess=sess, datatype='dwi_STE', root=cfg['data_path'] , 
+                      folderlevel='derivatives', workingdir=cfg['prep_foldername'],description='STE_fwd')
+        bids_strc_reg_ste  = create_bids_structure(subj=subj, sess=sess, datatype='registration', description='STE_To_LTE_'+data_type_LTE+'_fwd', root=cfg['data_path'], 
+                                       folderlevel='derivatives', workingdir=cfg['analysis_foldername'])
+        bids_strc_reg_ste.set_param(base_name='')
+        bvals_STE = read_numeric_txt(find_files_with_pattern(bids_STE,'bvalsNom')[0])[0]
+        files = find_files_with_pattern(bids_STE,'norm_')
+        files = [f for f in files if '_in_LTE' not in f]
+        selected_files = []
+        for bval in bvals_STE:
+            bval_int = int(bval * 1000)  
+            for f in files:
+                filename = os.path.basename(f)
+                if f"_{bval_int}" in filename:
+                    selected_files.append(f)       
+        S_S0_STE = np.zeros([S_S0_LTE.shape[0],S_S0_LTE.shape[1],S_S0_LTE.shape[2],bvals_STE.shape[0]]) 
+        k=0
+        for f in selected_files:
+            ants_apply_transforms_simple([f],  # input
+                                bids_LTE_prep.get_path('b0_dn_gc_ec_avg_bc_brain.nii.gz'),# reference
+                                [f.replace('.nii.gz','_in_LTE.nii.gz')],  # output
+                                [bids_strc_reg_ste.get_path('STE2dwi0GenericAffine.mat'), 0])  # transform 1
+            S_S0_STE[:, :, :, k]  = nib.load(f.replace('.nii.gz','_in_LTE.nii.gz')).get_fdata()
+            k += 1
+    
+              
+        ###  Get atlas in dwi space and atlas labels ### 
+        bids_strc_reg  = create_bids_structure(subj=subj, sess=1, datatype='registration', description=cfg['atlas']+'_To_'+data_type_LTE+'_fwd', root=cfg['data_path'] , 
+                                     folderlevel='derivatives', workingdir=cfg['analysis_foldername'])
+        bids_strc_reg.set_param(base_name='')
+        atlas = bids_strc_reg.get_path('atlas_in_dwi.nii.gz')
+        atlas_labels = pd.read_csv(
+             glob.glob(os.path.join(cfg['common_folder'], cfg['atlas'], '*atlas.label'))[0],
+             sep=r'\s+',
+             skiprows=14,  
+             header=None,  
+             names=['IDX', 'R', 'G', 'B', 'A', 'VIS', 'MSH', 'LABEL'], 
+             quotechar='"',)  
+
+        
+        fig, axs = plt.subplots(1, len( cfg['ROIs']), figsize=(12, 4))  
+        fig.subplots_adjust(wspace=0.05,hspace=0.02, top=0.90, bottom=0.14, left=0.09, right=0.95)  
+   
+        
+        ### PLOT Signal FOR EACH ROI ###
+        k=0
+        for ROI in cfg['ROIs']:
+     
+            print('Working on ' + ROI + '...')
+
+            mask_indexes = create_ROI_mask(atlas, atlas_labels, ROI, bids_strc_reg)
+
+            # Mask LTE data 
+            masked_LTE = S_S0_LTE[mask_indexes > 0]  # Select only voxels inside the ROI
+            masked_LTE = masked_LTE.reshape(-1, S_S0_LTE.shape[-1])  # Reshape to (n_voxels, n_bvals)
+            
+            # Remove bad voxels (NaN or 0 in any bval)
+            good_voxels_mask = ~(np.isnan(masked_LTE).any(axis=1) | (masked_LTE == 0).any(axis=1))
+            masked_LTE = masked_LTE[good_voxels_mask]
+            
+            # Average across good voxels
+            filtered_LTE = np.nanmean(masked_LTE, axis=0)
+            
+            # Insert b0
+            filtered_bvals = bvals_LTE
+            filtered_bvals = np.insert(filtered_bvals, 0, 0)
+            filtered_LTE = np.insert(filtered_LTE, 0, 1)
+            
+            # Define data to plot
+            data = np.log(filtered_LTE)
+            #data = filtered_LTE
+
+            # Plot data
+            axs[k].plot(np.transpose(filtered_bvals), data, 'ko', markersize=3)
+
+            # Fit LTE 
+            coeffs = np.polyfit(np.transpose(filtered_bvals), data, 3)
+            model = np.poly1d(coeffs)
+            b_fit = np.linspace(0,bvals_LTE.max(), 100)  # Smooth b-values 
+            y_fit = model(b_fit)
+            axs[k].plot(b_fit, y_fit, linestyle="--", color="black",label='_nolegend_')
+    
+            # Plot MD
+            masked_MD = MD[mask_indexes > 0]
+            masked_MD = masked_MD.reshape(-1, 1)  # Reshape to (n_voxels, n_bvals)
+            good_voxels_mask = ~(np.isnan(masked_MD).any(axis=1) | (masked_MD == 0).any(axis=1))
+            masked_MD = masked_MD[good_voxels_mask]
+            masked_MD = np.nanmean(masked_MD, axis=0)
+ 
+            ln_signal_exp = -b_fit*masked_MD
+            axs[k].plot(b_fit, ln_signal_exp, linestyle="-", color="red",label='_nolegend_')
+
+              
+            # Mask STE data 
+            masked_STE = S_S0_STE[mask_indexes > 0]  # Select only voxels inside the ROI
+            masked_STE = masked_STE.reshape(-1, S_S0_STE.shape[-1])  # Reshape to (n_voxels, n_bvals)
+            
+            # Remove bad voxels (NaN or 0 in any bval)
+            good_voxels_mask = ~(np.isnan(masked_STE).any(axis=1) | (masked_STE == 0).any(axis=1))
+            masked_STE = masked_STE[good_voxels_mask]
             
            
-            ### PLOT Signal FOR EACH ROI ###
-            k=0
-            for ROI in cfg['ROIs']:
- 
-                mask_indexes = create_ROI_mask(atlas, atlas_labels, ROI, bids_strc_reg)
-                
-                # LTE signal
-                if sess == 1:
-                    
-                    # Get values
-                    S_S0_masked = copy.deepcopy(S_S0_LTE)
-                    for v in range(S_S0_masked.shape[-1]):
-                        S_S0_masked[:, :, :, v] = np.multiply(S_S0_masked[:, :, :, v], mask_indexes)
-                    LTE = S_S0_masked.reshape(S_S0_masked.shape[0]*S_S0_masked.shape[1]*S_S0_masked.shape[2], S_S0_masked.shape[3])
-                    LTE = LTE[~(np.isnan(LTE).any(axis=1) | (LTE == 0).any(axis=1))]
-                    mask = bvals_LTE < 8
-                    filtered_bvals = bvals_LTE[mask]
-                    mask_diff = np.arange(0, 5)
-                    filtered_bvals = filtered_bvals[mask_diff]
-                    filtered_bvals = np.insert(filtered_bvals, 0, 0)
-                    filtered_LTE = np.nanmean(LTE, axis=0)
-                    filtered_LTE = np.expand_dims(filtered_LTE, axis=0)  
-                    filtered_LTE = filtered_LTE[mask]
-                    filtered_LTE = filtered_LTE[mask_diff]
-                    filtered_LTE = np.insert(filtered_LTE, 0, 1)
-                    
-                    data = np.log(filtered_LTE)
-                    #data = filtered_LTE
-                     
-                    # Plot data
-                    axs[k].plot(np.transpose(filtered_bvals), data, 'ko', markersize=3)
+            # Average across good voxels
+            filtered_STE = np.nanmean(masked_STE, axis=0)
+            
+            # Insert b0
+            filtered_bvals = bvals_STE
+            filtered_bvals = np.insert(filtered_bvals, 0, 0)
+            filtered_STE = np.insert(filtered_STE, 0, 1)
+            
+            # Define data to plot
+            data = np.log(filtered_STE)
+            #data = filtered_STE
 
-                    # Fit LTE 
-                    coeffs = np.polyfit(np.transpose(filtered_bvals), data, 2)
-                    model = np.poly1d(coeffs)
-                    b_fit = np.linspace(0,bvals_LTE.max(), 100)  # Smooth b-values 
-                    y_fit = model(b_fit)
-                    axs[k].plot(b_fit, y_fit, linestyle="--", color="black",label='_nolegend_')
+            # Fit STE 
+            # popt, pcov = curve_fit(linear_model, np.transpose(filtered_bvals), data) 
+            # b_fit = np.linspace(0, 3, 100)  # Smooth b-values 
+            # signal_fit = linear_model(b_fit, *popt)
+            coeffs = np.polyfit(np.transpose(filtered_bvals), data, 2)
+            model = np.poly1d(coeffs)
+            b_fit = np.linspace(0,bvals_STE.max(), 100)  # Smooth b-values 
+            y_fit = model(b_fit)
+            
+            # if sess == 1:
+            axs[k].plot(np.transpose(filtered_bvals), data, 'bo', markersize=3)
+            #axs[k].plot(np.transpose(bvals_STE), (np.nanmean(STE, axis=0)), 'bo', markersize=3)
+            axs[k].plot(b_fit, y_fit, linestyle="--", color="blue",label='_nolegend_')
 
-      
-                # STE signal
-                
-                # Get values
-                S_S0_masked = copy.deepcopy(S_S0_STE)
-                for v in range(S_S0_masked.shape[-1]):
-                    S_S0_masked[:, :, :, v] = np.multiply(S_S0_masked[:, :, :, v], mask_indexes)
-                STE = S_S0_masked.reshape(S_S0_masked.shape[0]*S_S0_masked.shape[1]*S_S0_masked.shape[2], S_S0_masked.shape[3])
-                STE = STE[~(np.isnan(STE).any(axis=1) | (STE == 0).any(axis=1))]
-                filtered_bvals = np.insert(bvals_STE, 0, 0)
-                filtered_STE= np.nanmean(STE, axis=0)
-                filtered_STE = np.expand_dims(filtered_STE, axis=0)  
-                filtered_STE = np.insert(filtered_STE, 0, 1)
-                
-                data = np.log(filtered_STE)
-                #data = filtered_STE
-                
-                # Fit STE 
-                popt, pcov = curve_fit(linear_model, np.transpose(filtered_bvals), data) 
-                b_fit = np.linspace(0, 3, 100)  # Smooth b-values 
-                signal_fit = linear_model(b_fit, *popt)
-                
-                if sess == 1:
-                    axs[k].plot(np.transpose(filtered_bvals), data, 'bo', markersize=3)
-                    #axs[k].plot(np.transpose(bvals_STE), (np.nanmean(STE, axis=0)), 'bo', markersize=3)
-                    axs[k].plot(b_fit, signal_fit, linestyle="--", color="blue",label='_nolegend_')
+            # if sess == 2:
+            #     axs[k].plot(np.transpose(filtered_bvals), data, 'ro', markersize=3)
+            #     #axs[k].plot(np.transpose(bvals_STE), (np.nanmean(STE, axis=0)), 'ro', markersize=3)
+            #     axs[k].plot(b_fit, signal_fit, linestyle="--", color="red",label='_nolegend_')
+    
+            
+            # Set axes
+            if k==0:
+                axs[k].set_ylabel(r'$ln(S / S_0)$', fontdict={'size': 12, 'weight': 'bold', 'style': 'italic'})  
+                axs[k].legend(['LTE','STE'], loc='upper right',prop={'size': 6})
+            axs[k].set_xlabel('b-val', fontdict={'size': 12, 'style': 'italic'})
+            axs[k].grid(True)
+            axs[k].set_title(ROI)
+            axs[k].set_xlim([-0.5, 3])
+            axs[k].set_ylim([-1.8, 0.1])
+            if k != 0:  # Remove y-axis labels for all but the first subplot
+                axs[k].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
+            #axs[k].set_xticks(np.unique(bvals_LTE))
+            
+         
+            k += 1
 
-                if sess == 2:
-                    axs[k].plot(np.transpose(filtered_bvals), data, 'ro', markersize=3)
-                    #axs[k].plot(np.transpose(bvals_STE), (np.nanmean(STE, axis=0)), 'ro', markersize=3)
-                    axs[k].plot(b_fit, signal_fit, linestyle="--", color="red",label='_nolegend_')
-
-                
-                # Set axes
-                if k==0:
-                    axs[k].set_ylabel(r'$S / S_0$', fontdict={'size': 12, 'weight': 'bold', 'style': 'italic'})  
-                    axs[k].legend(['LTE','STE - 16 avg','STE - 64 avg'], loc='upper right',prop={'size': 6})
-                axs[k].set_xlabel('b-val', fontdict={'size': 12, 'style': 'italic'})
-                axs[k].grid(True)
-                axs[k].set_title(ROI)
-                axs[k].set_xlim([-0.5, 3])
-                #axs[k].set_ylim([-1.6, 0.05])
-                if k != 0:  # Remove y-axis labels for all but the first subplot
-                    axs[k].tick_params(axis='y', which='both', left=False, right=False, labelleft=False)
-                #axs[k].set_xticks(np.unique(bvals_LTE))
-                
-             
-                k += 1
-        
            
 
     plt.savefig(os.path.join(output_folder,'STE_vs_LTE.png'))
