ecb0963ba0c2bbc257190f14cf82c054b4c68d35
Rita Oliveira <o_anarita@hotmail.com>
2025-05-14 13:29:25 +0200

    add denoise with matlab version of tMPPPCA

--- processing_dwi/Step3_preproc.py
+++ processing_dwi/Step3_preproc.py
@@ -232,6 +232,9 @@ def Step3_preproc(subj_list, cfg):
                                 denoise_vols_default_kernel(bids_strc.get_path('dwi.nii.gz'), bids_strc.get_path('dwi_dn.nii.gz'), bids_strc.get_path('dwi_dn_sigma.nii.gz'))
                             elif cfg['algo_denoising']=='tMPPCA':
                                 denoise_designer(bids_strc.get_path('dwi.nii.gz'), bids_strc.get_path('bvecs.txt'), bids_strc.get_path('bvalsNom.txt'), bids_strc.get_path('dwi_dn.nii.gz'), data_path, 'jespersen')
+                            # elif cfg['algo_denoising']=='tMPPCA_5D':
+                            #     denoise_matlab(paths_dwi_fwd, bids_strc.get_path('dwi_dn.nii.gz'), bids_strc.get_path('dwi.nii.gz'), cfg['code_path2'])
+                          
                             calc_snr(bids_strc.get_path('dwi.nii.gz'), bids_strc.get_path('dwi_dn_sigma.nii.gz'),bids_strc.get_path('dwi_snr.nii.gz'))
                             output_path = bids_strc.get_path();
                             QA_plotbvecs(bids_strc.get_path('bvecs.txt'), bids_strc.get_path('bvalsNom.txt'),os.path.join(output_path, 'QA_acquisition'))
@@ -289,8 +292,13 @@ def Step3_preproc(subj_list, cfg):
                 if not os.path.exists(bids_strc.get_path('dwi_dn.nii.gz')) or cfg['redo_denoise']:
                     if cfg['algo_denoising']=='MPPCA':
                         denoise_vols_default_kernel(bids_strc.get_path('dwi.nii.gz'), bids_strc.get_path('dwi_dn.nii.gz'), bids_strc.get_path('dwi_dn_sigma.nii.gz'))
-                    if cfg['algo_denoising']=='tMPPCA':
+                    elif cfg['algo_denoising']=='tMPPCA':
                         denoise_designer(bids_strc.get_path('dwi.nii.gz'), bids_strc.get_path('bvecs.txt'), bids_strc.get_path('bvalsNom.txt'), bids_strc.get_path('dwi_dn.nii.gz'), data_path, 'jespersen')
+                    elif cfg['algo_denoising']=='tMPPCA_5D' and os.path.basename(output_path)=='allDelta-allb':
+                        denoise_matlab(paths_dwi_fwd, bids_strc.get_path('dwi_dn.nii.gz'), bids_strc.get_path('dwi.nii.gz'), cfg['code_path2'])
+                    elif cfg['algo_denoising']=='tMPPCA_5D' and not os.path.basename(output_path)=='allDelta-allb':
+                        denoise_designer(bids_strc.get_path('dwi.nii.gz'), bids_strc.get_path('bvecs.txt'), bids_strc.get_path('bvalsNom.txt'), bids_strc.get_path('dwi_dn.nii.gz'), data_path, 'jespersen')
+
                     calc_snr(bids_strc.get_path('dwi_dn.nii.gz'), bids_strc.get_path('dwi_dn_sigma.nii.gz'),bids_strc.get_path('dwi_snr.nii.gz'))
                     QA_denoise(bids_strc, 'dwi_dn_res.nii.gz','dwi_dn_sigma.nii.gz',os.path.join(output_path, 'QA_denoise'))
 
