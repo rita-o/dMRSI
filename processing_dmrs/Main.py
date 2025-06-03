544d332908e3becb7f9b7b7c5cb568fb71324024
Malte Brammerloh <m.brammerloh@gmail.com>
2025-06-03 17:14:38 +0200

    Added new 9.4 T basis to the repo, together with removal of rounding error of dwell time.
    Analyzed 9.4 T data with single and dynamic fits. Found bug in dynamic fit config, which would always use callaghan model. Now back to single config files per model.

--- processing_dmrs/Main.py
+++ processing_dmrs/Main.py
@@ -34,24 +34,24 @@ cfg                         = {}
 cfg['data_path']            = os.path.join('/media','localadmin','DATA','data','20250424')
 cfg['water_ref_seq_number'] = 41
 cfg['min_seq_number']       = 42
-cfg['max_seq_number']       = 62
+cfg['max_seq_number']       = 41
 cfg['fixed_phase_shift']    = 245
 cfg['prep_foldername']      = 'preprocessed'
 cfg['coil_combination_method'] = 'Bruker method' # 'FSL MRS'
 cfg['analysis_foldername']  = 'analysis'
 cfg['common_folder']        = os.path.join(dmrsi_path,'common')
-cfg['basis_filename']       = os.path.join(cfg['common_folder'], 'mrs_basis')
+cfg['basis_filename']       = os.path.join(cfg['common_folder'], 'mrs_basis_sets', '9_4T_dSPECIAL_mrs_basis','fsl_mrs_fixed')
 cfg['diffusion_times']      = 'all'
 cfg['atlas']                = 'Atlas_WHS_v4'
-cfg['diffusion_models']     =  ['exp']#['biexp','callaghan','dki','exp'] # #[]
+cfg['diffusion_models']     =  ['biexp','callaghan','dki','exp'] #['exp'] #
 cfg['ppm_lim']              = [.2, 4.3]
-cfg['baseline']             = 'poly, 0' # 'spline, moderate' #
+cfg['baseline']             = 'poly, 4' # 'spline, moderate' #
 cfg['save_fit']             = True
 cfg['model']                = 'free_shift'
 
-#### STEP 1. Fitting of data >>> Use fsl_mrs env
-Step0_convert_bruker(subj_list, cfg)
+#### STEP 0. Converting bruker data
+#Step0_convert_bruker(subj_list, cfg)
 #Step0_combine_diffusion_times(subj_list, cfg)
 
 #### STEP 1. Fitting of data >>> Use fsl_mrs env
-#Step1_Fitting(subj_list, cfg)
+Step1_Fitting(subj_list, cfg)
--- processing_dmrs/Step1_Fitting.py
+++ processing_dmrs/Step1_Fitting.py
@@ -122,6 +122,7 @@ def Step1_Fitting(subj_list, cfg):
                     diffusion_times.append(dmrs.split('TD_')[-1].split('_dmrs.nii.gz')[0])
 
             for diffusion_time in diffusion_times:
+                print('Diffusion time: ' + diffusion_time)
                 bids_strc = create_bids_structure(subj=subj, sess=sess, datatype='dmrs', root=data_path,
                                                   folderlevel='derivatives', workingdir='preprocessed')
 
@@ -133,6 +134,7 @@ def Step1_Fitting(subj_list, cfg):
                 bvals_filename = bids_strc.get_path('bvals')
 
                 for diffusion_model in cfg['diffusion_models']:
+                    print('Diffusion model: ' + diffusion_model)
                     # Create output path
                     bids_strc.set_param(workingdir=cfg['analysis_foldername'], description='dyn_fit_'+diffusion_model+'_TD_'+str(diffusion_time))
                     out_path    = bids_strc.get_path()
@@ -140,7 +142,7 @@ def Step1_Fitting(subj_list, cfg):
                         os.makedirs(out_path)
 
                     # Create FSL MRS config file path
-                    mrs_dyn_config_filename = os.path.join(cfg['common_folder'],'mrs_dyn_config_multi.py')
+                    mrs_dyn_config_filename = os.path.join(cfg['common_folder'],f'mrs_dyn_config_{diffusion_model}.py')
 
                     # Check that the basis has the right phase/frequency convention
                     for mrs in dmrs_list:
@@ -176,7 +178,7 @@ def Step1_Fitting(subj_list, cfg):
                     create_directory(out_path)
                     dobj.save(out_path)#, save_dyn_obj=args.full_save)
 
-                    splot.plot_fit(data_to_fit, res, out=os.path.join(out_path,f'TD_{diffusion_time}_dyn_fit.png'))
+                    #splot.plot_fit(data_to_fit, res, out=os.path.join(out_path,f'TD_{diffusion_time}_dyn_fit.png'))
                     report.create_dynmrs_report(
                         dres,
                         fidfile=data_filename,
@@ -209,3 +211,4 @@ def Step1_Fitting(subj_list, cfg):
                             dim_tags=data.dim_tags,
                             affine=affine)
                         pred.save(os.path.join(out_path , f'TD_{diffusion_time}_fit.nii.gz'))
+                print('')
